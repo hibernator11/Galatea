f821f6c29f90b2bc46b31c3fed3d0c30e0dbd254
Gcandela <gustavo.candela@cervantesvirtual.com>
2016-05-26 10:40:37 +0200

    users count admin

--- public/dist/application.js
+++ public/dist/application.js
@@ -5815,14 +5815,12 @@ angular.module('users.admin.routes').config(['$stateProvider',
     $stateProvider
       .state('admin.users', {
         url: '/users',
-        //templateUrl: 'modules/users/client/views/admin/list-users.client.view.html',
         templateUrl: 'modules/users/client/views/admin/dashboard.list-users.client.view.html',
         controller: 'UserListController'
       })
       .state('admin.user', {
         url: '/users/:userId',
         templateUrl: 'modules/users/client/views/admin/dashboard.view-user.client.view.html',
-        //templateUrl: 'modules/users/client/views/admin/view-user.client.view.html',
         controller: 'UserController',
         resolve: {
           userResolve: ['$stateParams', 'Admin', function ($stateParams, Admin) {
@@ -5834,7 +5832,6 @@ angular.module('users.admin.routes').config(['$stateProvider',
       })
       .state('admin.user-edit', {
         url: '/users/:userId/edit',
-        //templateUrl: 'modules/users/client/views/admin/edit-user.client.view.html',
         templateUrl: 'modules/users/client/views/admin/dashboard.edit-user.client.view.html',
         controller: 'UserController',
         resolve: {
